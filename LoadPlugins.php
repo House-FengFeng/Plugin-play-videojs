65fe45c16bbe942de344bb5d1467ba466e9ce2e9
Zhū Jǐn Fēng <vnxtc123@gmail.com>
2015-11-23 23:06:03 +0700

    Create LoadPlugins.php

---
+++ LoadPlugins.php
@@ -0,0 +1,14 @@
+<?php
+### Version: 1.0.0.0
+### Author: Jkey C Phong
+
+if (!defined('Check')) {
+	header('HTTP/1.1 404 Not Found');
+	exit();
+}
+### Load Plugin Get Link ###
+$Plugins_List = array();
+$Plugins_List[] = 'Class-Picasa.php';
+$Plugins_List[] = 'Class-Youtube.php';
+### Load Plugin Get Link ###
+?>
