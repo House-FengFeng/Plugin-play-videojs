17797d46e877d763e4dd10e15b79b79a4f9154b0
Zhū Jǐn Fēng <vnxtc123@gmail.com>
2015-11-23 23:05:21 +0700

    Trang index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+### Version: 1.0.0.0
+### Author: Jkey C Phong
+
+### Trang index.php ###
+
+require 'Player_Load.php';		// Load plugin
+$Object = new Player();
+$Object->__Show('https://picasaweb.google.com/102301520469286682879/Inuyasha?authkey=Gv1sRgCLPfu9qS8tKnnQE#6219549412764126098');	// Link phim
+unset($Object); // Nếu không dùng nữa, hãy unset để giải phóng bộ nhớ
+
+### Trang index.php ###
+
+?>
