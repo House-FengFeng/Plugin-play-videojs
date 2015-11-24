b548b584c73f790a04a8e471343a1d978583a653
Zhū Jǐn Fēng <vnxtc123@gmail.com>
2015-11-24 22:18:38 +0700

    Update | Version: 2.0.0.0

--- Player_Load.php
+++ Player_Load.php
@@ -4,16 +4,18 @@ require_once 'Player.php';
 require_once 'Lib/Embed.php';
 require_once 'LoadPlugins.php';
 
+### Version: 2.0.0.0 ###
+
 Class Player extends Player_Load
 {
 	Public Function __Show($Url)
 	{
-    	if (isset($GLOBALS['css'], $GLOBALS['js'], $GLOBALS['ie8'], $GLOBALS['text'], $GLOBALS['quality_css'], $GLOBALS['quality_js'], $GLOBALS['width'], $GLOBALS['height'], $GLOBALS['flash']) === TRUE) {
+    	if (isset($GLOBALS['css'], $GLOBALS['js'], $GLOBALS['ie8'], $GLOBALS['text'], $GLOBALS['width'], $GLOBALS['height'], $GLOBALS['flash'], $GLOBALS['quality_js'], $GLOBALS['quality_css']) === TRUE) {
 			parent::_Embed();
 			parent::_Player($Url);
 		}
 		else {
-			exit('Lỗi trong quá trình trao đổi dữ liệu');
+			exit('L&#7895;i trong qu&aacute; tr&igrave;nh trao &#273;&#7893;i d&#7919; li&#7879;u');
 		}
 	}
 }
