1767acdf927d54e1940540660bc921d20a5d4296
Zhū Jǐn Fēng <vnxtc123@gmail.com>
2015-11-25 17:46:46 +0700

    Update | Version: 2.0.0.1

--- Player_Load.php
+++ Player_Load.php
@@ -1,16 +1,13 @@
 <?php
-define('Check', true);
 require_once 'Player.php';
 require_once 'Lib/Embed.php';
 require_once 'LoadPlugins.php';
-
-### Version: 2.0.0.0 ###
 
 Class Player extends Player_Load
 {
 	Public Function __Show($Url)
 	{
-    	if (isset($GLOBALS['css'], $GLOBALS['js'], $GLOBALS['ie8'], $GLOBALS['text'], $GLOBALS['width'], $GLOBALS['height'], $GLOBALS['flash'], $GLOBALS['quality_js'], $GLOBALS['quality_css']) === TRUE) {
+    	if (isset($GLOBALS['css'], $GLOBALS['js'], $GLOBALS['ie8'], $GLOBALS['text'], $GLOBALS['width'], $GLOBALS['height'], $GLOBALS['flash'], $GLOBALS['quality_js'], $GLOBALS['quality_css'], $GLOBALS['Cache']) === TRUE) {
 			parent::_Embed();
 			parent::_Player($Url);
 		}
