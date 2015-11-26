519c6af1ba3578716bd6051af39f6cae8cc7935c
Zhū Jǐn Fēng <vnxtc123@gmail.com>
2015-11-26 23:25:50 +0700

    Update | Version: 2.0.0.2

--- LoadPlugins.php
+++ LoadPlugins.php
@@ -1,12 +1,11 @@
 <?php
 
-### Version: 2.0.0.0 ###
-### Tác giả: Jkey C Phong ###
-### Vui lòng không edit code dưới mọi hình thức! ###
+### Update ver: 2.0.0.2
 
 ### Load Plugin Get Link ###
 $Plugins_List = array();
 $Plugins_List[] = 'Class-Picasa.php';
+//$Plugins_List[] = 'PHP 5.3/Class-Picasa.php';	// Sử dụng cho những bản PHP ver 5.3
 $Plugins_List[] = 'Class-Youtube.php';
 $Plugins_List[] = 'Class-Zing.php';
 ### Load Plugin Get Link ###
