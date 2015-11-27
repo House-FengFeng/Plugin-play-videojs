98d39f72b2604ec7233049f621c39bc0226698c7
Zhū Jǐn Fēng <vnxtc123@gmail.com>
2015-11-27 17:57:12 +0700

    Update | Version: 3.0.0

--- LoadPlugins.php
+++ LoadPlugins.php
@@ -1,12 +1,15 @@
 <?php
 
-### Update ver: 2.0.0.2
+### Update ver 3.0.0 ###
 
 ### Load Plugin Get Link ###
 $Plugins_List = array();
-$Plugins_List[] = 'Class-Picasa.php';
+
 //$Plugins_List[] = 'PHP 5.3/Class-Picasa.php';	// Sử dụng cho những bản PHP ver 5.3
-$Plugins_List[] = 'Class-Youtube.php';
-$Plugins_List[] = 'Class-Zing.php';
+//$Plugins_List[] = 'Class-Picasa.php';
+//$Plugins_List[] = 'Class-Youtube.php';
+//$Plugins_List[] = 'Class-Zing.php';
+//$Plugins_List[] = 'Class-Dailymotion.php';
+//$Plugins_List[] = 'Class-Photo.php';
 ### Load Plugin Get Link ###
 ?>
