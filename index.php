4c07dde2391c9cfb69acd66185a4ea6f49df4235
Zhū Jǐn Fēng <vnxtc123@gmail.com>
2015-11-27 17:52:08 +0700

    Update | Version: 3.0.0

--- index.php
+++ index.php
@@ -18,14 +18,16 @@
 
 	<?php
 
-	### Hỗ trợ 3 server là: Picasa, Youtube, Zing TV (Hỗ trợ cho cả host nước ngoài bị chặn IP)
-	### Version: 2.0.0.2
+	### Hỗ trợ 5 server là: Picasa, Youtube, Dailymotion, Google Photo, Zing TV (Hỗ trợ cho cả host nước ngoài bị chặn IP)
+	### Version: 3.0.0
 
 	require 'Player_Load.php';
 	$Object = new Player();
-	$Object->__Show('https://picasaweb.google.com/111679342576603394295/Bask?authkey=Gv1sRgCKqoyvGZ_aGN1gE#6153409278111438402');
+	//$Object->__Show('https://picasaweb.google.com/111679342576603394295/Bask?authkey=Gv1sRgCKqoyvGZ_aGN1gE#6153409278111438402', 'sub/sub.srt');
 	//$Object->__Show('http://tv.zing.vn/video/Haikyuu-Tap-25-END/IWZAC066.html');
-	//$Object->__Show('https://www.youtube.com/watch?v=oClscAn5ou4');
+	//$Object->__Show('https://www.youtube.com/watch?v=oClscAn5ou4','sub/sub.srt', 'capture.png');
+	$Object->__Show('http://www.dailymotion.com/video/x3azrmi_adele-hello-cover-by-alice-olivia_music');
+	//$Object->__Show('https://photos.google.com/share/AF1QipPP_M4EaHgKVuzpgaTqK-Ql-flIWITx5QF-hfPzLuR0SdC971MQYYhLN-_8yaQulg/photo/AF1QipPhlqVQlq0VT-6fPeKH7ogcZZ7f94OJu_klKKyz?key=N2dCVDJLd1NCREJFQnM3WEpUNDItMDhxWmtpZzdn');
 	unset($Object);
 	?>
 </body>
