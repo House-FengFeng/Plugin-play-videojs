7ebfb2437b99d06f69d4ae68365d600000735093
Zhū Jǐn Fēng <vnxtc123@gmail.com>
2015-11-27 17:53:01 +0700

    Update | Version: 3.0.0

--- Player_Load.php
+++ Player_Load.php
@@ -1,4 +1,7 @@
 <?php
+
+### Ver: 3.0.0 ###
+
 require_once 'Player.php';
 require_once 'Lib/Embed.php';
 require_once 'LoadPlugins.php';
@@ -8,6 +11,8 @@ Class Player extends Player_Load
 	Private Function __Options()
 	{
 		if (isset(
+				$GLOBALS['logo'],
+				$GLOBALS['des_logo'],
 				$GLOBALS['text'],
 				$GLOBALS['width'],
 				$GLOBALS['height'],
@@ -19,11 +24,11 @@ Class Player extends Player_Load
 		}
 	}
 
-	Public Function __Show($Url, $Sub = '')
+	Public Function __Show($Url, $Sub = '', $Poster = '')
 	{
 		$this->__Options();
 		parent::_Embed();
-		parent::_Player($Url, $Sub);
+		parent::_Player($Url, $Sub, $Poster);
 	}
 }
 ?>
