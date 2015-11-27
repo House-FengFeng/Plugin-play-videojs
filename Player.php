1f523405752121b45d1259b4ae3804bf94cab07b
Zhū Jǐn Fēng <vnxtc123@gmail.com>
2015-11-27 17:56:28 +0700

    Update | Version: 3.0.0

--- Player.php
+++ Player.php
@@ -6,14 +6,14 @@
 
 ### Chỉnh sửa các tùy chọn cho player videojs ###
 $GLOBALS['download']		= 1;				// Bật chế độ download
-$GLOBALS['logo']			= 'logo.png';		// Chứa link logo, nếu không sử dụng logo hãy để trống
+$GLOBALS['logo']		= 'logo.png';		// Chứa link logo, nếu không sử dụng logo hãy để trống
 $GLOBALS['des_logo']		= 'https://www.facebook.com/profile.php?id=100004989074517';
 (int)$GLOBALS['width']		= '1000';			// Chiều rộng của player
-(int)$GLOBALS['height']		= '500';			// Chiều cao của player
-$GLOBALS['text']			= 'Kh&ocirc;ng h&#7895; tr&#7907; HTML5';		// Text hiển thị khi trình duyệt của người dùng không hỗ trợ HTML5
+(int)$GLOBALS['height']	= '500';			// Chiều cao của player
+$GLOBALS['text']		= 'Kh&ocirc;ng h&#7895; tr&#7907; HTML5';		// Text hiển thị khi trình duyệt của người dùng không hỗ trợ HTML5
 (bool)$GLOBALS['autoplay']	= 'true';	// Thay đổi thành true hay false
-$GLOBALS['preload']			= 'auto';	// Nên để mặc định là auto
-(float)$GLOBALS['playbackRates'] = '0.5, 1, 1.5, 2';	// Tốc độ khi play video
+$GLOBALS['preload']		= 'auto';	// Nên để mặc định là auto
+(float)$GLOBALS['playbackRates'] = '0.5, 1, 1.5, 2, 3';	// Tốc độ khi play video
 ### Chỉnh sửa các tùy chọn cho player videojs ###
 
 ### Cấu hình cache link phim ###
